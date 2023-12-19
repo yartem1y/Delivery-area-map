9f3f7725418c16a6d817548238fbd0b65d5e6933
Artemiy Yakovlev <154023177+yartem1y@users.noreply.github.com>
2023-12-19 23:29:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 ymaps.ready(init);
 
-function init() {
-    var myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64], // центр карты
-        zoom: 10 // масштаб карты
-    });
+        function init() {
+            var coordinates = [
+                [55.751244, 37.618423],  // пример координат точки 1
+                [55.751831, 37.619282],  // пример координат точки 2
+                // Добавьте координаты других точек
+            ];
 
-    var myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-        hintContent: 'Москва!',
-        balloonContent: 'Столица России'
-    });
+            var map = new ymaps.Map("map", {
+                center: coordinates[0],  // центр карты
+                zoom: 15  // масштаб карты
+            });
 
-    myMap.geoObjects.add(myPlacemark);
-}
+            // Добавляем маркеры для каждой точки
+            for (var i = 0; i < coordinates.length; i++) {
+                var marker = new ymaps.Placemark(coordinates[i]);
+                map.geoObjects.add(marker);
+            }
+
+            // Создаем ломаную линию
+            var polyline = new ymaps.Polyline(coordinates, {}, {
+                strokeColor: '#0000FF',  // цвет линии
+                strokeWidth: 5  // ширина линии
+            });
+
+            map.geoObjects.add(polyline);
+
+            // Создаем полигон
+            var polygonGeometry = new ymaps.geometry.Polygon([coordinates], {});
+            var polygon = new ymaps.Polygon([polygonGeometry.getCoordinates()], {
+                fillColor: '#00FF00',  // цвет заливки
+                strokeColor: '#0000FF',  // цвет линии
+                strokeWidth: 5  // ширина линии
+            });
+
+            map.geoObjects.add(polygon);
+        }
