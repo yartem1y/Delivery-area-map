e5021549a47b6326a4776c2d3739ac6228dbdeb6
Artemiy Yakovlev <154023177+yartem1y@users.noreply.github.com>
2023-12-19 23:13:15 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,6 @@
+import folium
+from coordinates import coordinates
+
+m = folium.Map(location=coordinates[0], zoom_start=15)
+
+folium.Polygon(locations=coordinates, color='blue', fill_color='green', fill_opacity=0.4).add_to(m)
