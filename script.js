252feba0dba7388017b1aac41386648f2a983e9c
Artemiy Yakovlev <154023177+yartem1y@users.noreply.github.com>
2023-12-20 00:11:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,39 @@
-ymaps.ready(init);
+ymaps.ready(function () {
+    // Асинхронная загрузка координат
+    loadCoordinates(function (coordinates) {
+        // Инициализация карты с центром и начальным масштабом
+        var map = new ymaps.Map("map", {
+            center: coordinates[0],
+            zoom: 15
+        });
 
-function init() {
-    // Подключаем координаты из файла coordinates.js
-    var coordinates = getCoordinates();
-
-    // Инициализация карты с центром и начальным масштабом
-    var map = new ymaps.Map("map", {
-        center: coordinates[0],
-        zoom: 15
-    });
-
-    // Добавление маркеров для каждой точки
-    for (var i = 0; i < coordinates.length; i++) {
-        var marker = new ymaps.Placemark(coordinates[i]);
-        map.geoObjects.add(marker);
-    }
-
-    // Создание ломаной линии
-    var polyline = new ymaps.Polyline(coordinates, {}, { color: 'blue' });
-    map.geoObjects.add(polyline);
-
-    // Создание полигона
-    var polygon = new ymaps.Polygon([coordinates], {}, { color: 'blue', fillColor: 'green', fillOpacity: 0.4 });
-    map.geoObjects.add(polygon);
-
-    // Удаление маркеров
-    map.geoObjects.each(function (geoObject) {
-        if (geoObject instanceof ymaps.Placemark) {
-            map.geoObjects.remove(geoObject);
+        // Добавление маркеров для каждой точки
+        for (var i = 0; i < coordinates.length; i++) {
+            var marker = new ymaps.Placemark(coordinates[i]);
+            map.geoObjects.add(marker);
         }
+
+        // Создание ломаной линии
+        var polyline = new ymaps.Polyline(coordinates, {}, { color: 'blue' });
+        map.geoObjects.add(polyline);
+
+        // Создание полигона
+        var polygon = new ymaps.Polygon([coordinates], {}, { color: 'blue', fillColor: 'green', fillOpacity: 0.4 });
+        map.geoObjects.add(polygon);
     });
-}
+});
 
-// Функция для загрузки координат из файла coordinates.js
-function getCoordinates() {
-    // Переменная, в которую будут загружены координаты
-    var coordinates = [];
-
+// Функция для асинхронной загрузки координат с использованием колбэка
+function loadCoordinates(callback) {
     // Асинхронная загрузка файла
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'coordinates.js', false); // false - синхронная загрузка
+    xhr.open('GET', 'coordinates.js', true); // true - асинхронная загрузка
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // Парсим содержимое файла coordinates.js как JSON
+            var coordinates = JSON.parse(xhr.responseText);
+            callback(coordinates);
+        }
+    };
     xhr.send();
-
-    // Если запрос выполнен успешно
-    if (xhr.status == 200) {
-        // Парсим содержимое файла coordinates.js как JavaScript код
-        // (в данном случае, предполагается, что файл содержит валидный массив координат)
-        eval(xhr.responseText);
-        coordinates = window.coordinates;
-    }
-
-    return coordinates;
 }
